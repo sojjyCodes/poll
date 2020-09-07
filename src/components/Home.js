ad51b7113a948d566fa190d8898b32eb8943cea9
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-07 18:37:40 +0100

    Made the poll work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import Poll from "react-polls";
 import "./home.css";
-function Home() {
-  return (
-    <div className="app">
-      <h1>Home Page</h1>
-    </div>
-  );
-}
 
-export default Home;
+const pollQuestions = "Is react-polls useful?";
+const pollAnswers = [
+  { opiton: "Yes", votes: 8 },
+  { option: "No", votes: 2 },
+];
+export default class Home extends Component {
+  state = {
+    pollAnswers: [...pollAnswers],
+  };
+
+  handleVote = (voteAnswer) => {
+    const { pollAnswers } = this.state;
+    const newPollAnswers = pollAnswers.map((answer) => {
+      if (answer.option === voteAnswer) answer.votes++;
+      return answer;
+    });
+    this.setState({
+      pollAnswers: newPollAnswers,
+    });
+  };
+
+  render() {
+    const { pollAnswers } = this.state;
+    return (
+      <div className="app">
+        <h1>Home Page</h1>
+        <Poll question={pollQuestions} answers={pollAnswers} onVote={this.handleVote} />
+      </div>
+    );
+  }
+}
