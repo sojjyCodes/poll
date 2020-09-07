bb76c7bca720cc1171d5db6f9938d34ba1dac7a1
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-08 00:29:15 +0100

    Working with the react-bootstrap Card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 import Poll from "react-polls";
 import "./home.css";
 
 function Home() {
   return (
     <div>
-      <div className="box">
-        <h3>This is the box</h3>
-      </div>
+      <Card
+        style={{
+          height: "30vh",
+          justifyContent: "center",
+          alignContent: "center",
+          width: "850px",
+          backgroundColor: "#a1a6a8",
+        }}
+      >
+        <Card.Body>
+          What is your prefered Javascript Framework for Web Development
+        </Card.Body>
+      </Card>
     </div>
   );
 }
