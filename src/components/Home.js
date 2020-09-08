230b69461947fc7d96bebf1a9178b1852290d1cb
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-08 18:10:38 +0100

    Some prettier stuffs

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,30 +5,37 @@ import "./home.css";
 import Result from "./Result";
 
 function Home() {
-  return (
-    <div>
-      <Card
-        bg="info"
-        border="success"
-        style={{
-          borderRadius: "10px",
-          margin: "50px",
-          height: "30vh",
-          width: "850px",
-          backgroundColor: "white",
-        }}
-      >
-        <Card.Header style={{ color: "black" }}>Web Design</Card.Header>
-        <Card.Body
-          style={{ fontSize: "20px", fontWeight: "bold", fontStyle: "italic" }}
-        >
-          What is your prefered Javascript Framework for Web Development
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card
+                bg="info"
+                border="success"
+                style={{
+                    borderRadius: "10px",
+                    margin: "50px",
+                    height: "30vh",
+                    width: "850px",
+                    backgroundColor: "white",
+                }}
+            >
+                <Card.Header style={{ color: "black" }}>Web Design</Card.Header>
+                <Card.Body
+                    style={{
+                        fontSize: "20px",
+                        fontWeight: "bold",
+                        fontStyle: "italic",
+                    }}
+                >
+                    What is your prefered Javascript Framework for Web
+                    Development
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">
+                        Last updated 3 mins ago
+                    </small>
+                </Card.Footer>
+            </Card>
+        </div>
+    );
 }
 export default Home;
--- src/components/Result.js
+++ src/components/Result.js
@@ -4,40 +4,40 @@ import "./home.css";
 
 const pollQuestions = "What is the best Javascript Framework?";
 const pollAnswers = [
-  { option: "React", votes: 40 },
-  { option: "Angular", votes: 10 },
-  { option: "Vuejs", votes: 35 },
-  { option: "None", votes: 3 },
+    { option: "React", votes: 40 },
+    { option: "Angular", votes: 10 },
+    { option: "Vuejs", votes: 35 },
+    { option: "None", votes: 3 },
 ];
 export default class Result extends Component {
-  state = {
-    pollAnswers: [...pollAnswers],
-  };
+    state = {
+        pollAnswers: [...pollAnswers],
+    };
 
-  handleVote = (voteAnswer) => {
-    const { pollAnswers } = this.state;
-    const newPollAnswers = pollAnswers.map((answer) => {
-      if (answer.option === voteAnswer) answer.votes++;
-      return answer;
-    });
-    this.setState({
-      pollAnswers: newPollAnswers,
-    });
-  };
+    handleVote = (voteAnswer) => {
+        const { pollAnswers } = this.state;
+        const newPollAnswers = pollAnswers.map((answer) => {
+            if (answer.option === voteAnswer) answer.votes++;
+            return answer;
+        });
+        this.setState({
+            pollAnswers: newPollAnswers,
+        });
+    };
 
-  render() {
-    const { pollAnswers } = this.state;
-    return (
-      <div className="app">
-        <Poll
-          question={pollQuestions}
-          answers={pollAnswers}
-          onVote={this.handleVote}
-          theme="purple"
-          noStorage
-          customStyles={{ theme: "purple" }}
-        />
-      </div>
-    );
-  }
+    render() {
+        const { pollAnswers } = this.state;
+        return (
+            <div className="app">
+                <Poll
+                    question={pollQuestions}
+                    answers={pollAnswers}
+                    onVote={this.handleVote}
+                    theme="purple"
+                    noStorage
+                    customStyles={{ theme: "purple" }}
+                />
+            </div>
+        );
+    }
 }
