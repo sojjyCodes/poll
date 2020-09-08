17100722cd20db18f68d3c1bb73113604fe29e30
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-08 18:10:38 +0100

    Some prettier stuffs

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,40 +6,40 @@ import Home from "./Home";
 import "./container.css";
 
 function Container() {
-  const User = "sojjyCodes";
+    const User = "sojjyCodes";
 
-  return (
-    <div className="app">
-      <Navbar variant="dark" style={{ backgroundColor: "#24292E" }}>
-        <Navbar.Brand href="#home">
-          sojjy's Poll
-          <IoMdPodium />
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#login" style={{ marginRight: "10px" }}>
-              Login
-            </Nav.Link>
-            <Nav.Link href="#signup">Sign Up</Nav.Link>
-            <NavDropdown title={User} id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#">
-                Logout <IoMdLogOut />
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    return (
+        <div className="app">
+            <Navbar variant="dark" style={{ backgroundColor: "#24292E" }}>
+                <Navbar.Brand href="#home">
+                    sojjy's Poll
+                    <IoMdPodium />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="#login" style={{ marginRight: "10px" }}>
+                            Login
+                        </Nav.Link>
+                        <Nav.Link href="#signup">Sign Up</Nav.Link>
+                        <NavDropdown title={User} id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#">
+                                Logout <IoMdLogOut />
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
 
-      <div className="component">
-        <h1 style={{ color: "rgb(44, 202, 250)", marginTop: "40px" }}>
-          Today's Selection <IoMdPodium />
-        </h1>
-        <Home />
-        <Home />
-      </div>
-    </div>
-  );
+            <div className="component">
+                <h1 style={{ color: "rgb(44, 202, 250)", marginTop: "40px" }}>
+                    Today's Selection <IoMdPodium />
+                </h1>
+                <Home />
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default Container;
