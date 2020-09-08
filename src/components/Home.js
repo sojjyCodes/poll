e5800ccd3a1c5c96ed8fc9113547b87606bc36af
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-08 14:38:53 +0100

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Image } from "react-bootstrap";
 import Poll from "react-polls";
 import "./home.css";
 import Result from "./Result";
@@ -8,18 +8,25 @@ function Home() {
   return (
     <div>
       <Card
+        bg="info"
+        border="success"
         style={{
           borderRadius: "10px",
           margin: "50px",
           height: "30vh",
           width: "850px",
-          backgroundColor: "#a1a6a8",
+          backgroundColor: "white",
         }}
       >
-        <Card.Title style={{ color: "green" }}>Web Design</Card.Title>
-        <Card.Body style={{fontSize: "25px", fontWeight: "bold", fontStyle: "italic"}}>
+        <Card.Header style={{ color: "black" }}>Web Design</Card.Header>
+        <Card.Body
+          style={{ fontSize: "20px", fontWeight: "bold", fontStyle: "italic" }}
+        >
           What is your prefered Javascript Framework for Web Development
         </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
       </Card>
     </div>
   );
