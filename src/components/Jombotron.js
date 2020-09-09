dc8ad9c88ccdc6d1b785ea2a4ba74469b75dce81
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-09 06:35:04 +0100

    Update

--- src/components/Jombotron.js
+++ src/components/Jombotron.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
-
+import "./jombotron.css";
 function Jombotron() {
     return (
         <Jumbo fluid className="jumbo">
-            <div className="overlay"></div>
-            <Container>
-                <h1>Sojjy's Poll</h1>
-                <p>A site we developers can come to answer polls</p>
-            </Container>
+            <div className="app__container">
+                <Container>
+                    <h1>Sojjy's Poll</h1>
+                    <p>A site we developers can come to answer polls</p>
+                </Container>
+            </div>
         </Jumbo>
     );
 }
