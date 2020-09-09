0f86f826be42f2debb9b7440d7f71ae54d81deeb
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-10 00:13:44 +0100

    working on the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,52 @@
-import React from "react";
-import { Container, Card, Button, Col, Row } from "react-bootstrap";
+import React, { Component } from "react";
 
-function Login() {
-    return (
-        <div>
-            <Container style={styles.container}>
-                <Card>
-                    <Card.Body>Login Page</Card.Body>
-                </Card>
-                
-            </Container>
-           
-        </div>
-    );
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="customCheck1"
+                        />
+                        <label
+                            className="custom-control-label"
+                            htmlFor="customCheck1"
+                        >
+                            Remember me
+                        </label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">
+                    Submit
+                </button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+        );
+    }
 }
-
-const styles = {
-    container: {
-        marginTop: "100px",
-    },
-};
-export default Login;
