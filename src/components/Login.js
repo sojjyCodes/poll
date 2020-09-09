24dbd36509a6acc7829f25bfade811cde5ff413d
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-10 00:34:20 +0100

    working on the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,67 @@
-import React, { Component } from "react";
+import React from "react";
+import { Container, Card, Button, Col, Row } from "react-bootstrap";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
+function Login() {
+    return (
+        <div>
+            <Container style={styles.container}>
+                <Card>
+                    <form>
+                        <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                    />
-                </div>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Enter email"
+                            />
+                        </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                    />
-                </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter password"
+                            />
+                        </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input
-                            type="checkbox"
-                            className="custom-control-input"
-                            id="customCheck1"
-                        />
-                        <label
-                            className="custom-control-label"
-                            htmlFor="customCheck1"
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input"
+                                    id="customCheck1"
+                                />
+                                <label
+                                    className="custom-control-label"
+                                    htmlFor="customCheck1"
+                                >
+                                    Remember me
+                                </label>
+                            </div>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block"
                         >
-                            Remember me
-                        </label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">
-                    Submit
-                </button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-        );
-    }
+                            Submit
+                        </button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </form>
+                </Card>
+            </Container>
+        </div>
+    );
 }
+
+const styles = {
+    container: {
+        marginTop: "100px",
+    },
+};
+export default Login;
