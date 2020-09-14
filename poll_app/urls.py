bb455aeb0178d24d03fcea0cfb0944d763918df9
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-14 08:46:46 +0100

    Switching back to the one i felt confortable with

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('polls.urls')),
     path('result/', TemplateView.as_view(template_name='index.html')),
     path('login/', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
