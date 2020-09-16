66fe3a1450c710ca6704bcf0527fd3ca8ef31bc5
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-16 18:46:59 +0100

    Updates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
+import random
+
 
 def sendmail(request):
+
+    ver_code = '1234567890'
+    number = 1
+    length = 5
+
+    for p in range(number):
+        password = ''
+    for code in range(length):
+        password += random.choice(ver_code)
+        print(password)
 
     send_mail(
         'Subject',
@@ -10,10 +22,9 @@ def sendmail(request):
         'You just attempted to reset your password, this attempt requires further verification. \n'
         'To complete the sign in,' 
         'enter the verification code on your device. \n '
-        'Verification code: 609147',
-        'from@example.com',
+        f'Verification code: {code}',
+        'sojjy.mail-6507d0@inbox.mailtrap.io',
         ['to@example.com'],
         fail_silently=False,
     )
-
     return HttpResponse('<h1>Please verify </h1>')
